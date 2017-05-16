392ea83e3c7718daaae27390d5b0ba5bae1e02ac
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2017-05-16 15:20:36 +0200

    Move annotations code in a separate file to avoid jumbo-sized kitchen sink 10 hour compile time source code files in the future.
    It’s surprising how fast you get to pretty long compile times when writing C++ code.

---
+++ LLVMFloatToFixed/LLVMFloatToFixedPass.h
@@ -0,0 +1,34 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/ADT/SmallPtrSet.h"
+
+
+#ifndef __LLVM_FLOAT_TO_FIXED_PASS_H__
+#define __LLVM_FLOAT_TO_FIXED_PASS_H__
+
+
+#define N_ANNO_VAR 32
+#define NO_FLOAT_ANNO "no_float"
+
+
+namespace flttofix {
+
+struct FloatToFixed : public llvm::ModulePass {
+  static char ID;
+
+  FloatToFixed(): ModulePass(ID) { }
+  bool runOnModule(llvm::Module &M) override;
+  llvm::SmallPtrSet<llvm::Value*, N_ANNO_VAR> readGlobalAnnotations(llvm::Module &m, bool functionAnnotation = false);
+  llvm::SmallPtrSet<llvm::Value*, N_ANNO_VAR> readLocalAnnotations(llvm::Function &f);
+  bool isValidAnnotation(llvm::ConstantExpr *expr);
+  llvm::SmallPtrSet<llvm::Value*, N_ANNO_VAR> removeNoFloatTy(llvm::SmallPtrSet<llvm::Value*, N_ANNO_VAR> res);
+  void printAnnotatedObj(llvm::Module &m);
+};
+
+}
+
+
+#endif
+
+
