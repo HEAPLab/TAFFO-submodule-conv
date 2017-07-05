HEAPLab/TAFFO-submodule-conv

commit e0e8b1e9e8c08d2c2c722f6d4db1253c8c8e2626

Add debug option
Fix a bug on fallback() with pointer operands

LLVMFloatToFixed/Conversion.cpp
@@ -10,7 +10,7 @@
 #include "llvm/ADT/APSInt.h"
 #include "llvm/ADT/APFloat.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/Debug.h"
+
 #include <cmath>
 #include <cassert>
 #include "LLVMFloatToFixedPass.h"
@@ -33,9 +33,10 @@ void FloatToFixed::performConversion(Module& m, const std::vector<Value*>& q)
     if (newv && newv != ConversionError) {
       convertedPool.insert({v, newv});
     } else {
-      errs() << "warning: ";
-      v->print(errs());
-      errs() << " not converted\n";
+      DEBUG(errs() << "warning: ";
+            v->print(errs());
+            errs() << " not converted\n";);
+
       convertedPool.insert({v, ConversionError});
     }
   }
@@ -65,6 +66,10 @@ Value *FloatToFixed::convertSingleValue(Module& m, DenseMap<Value *, Value *>& o
     if (instr->isBinaryOp()) {
       res = convertBinOp(operandPool,instr);
     } else if (instr->isCast()){
+      /*le istruzioni Instruction::
+        [Trunc,ZExt,SExt,FPToUI,FPToSI,UIToFP,SIToFP,FPTrunc,FPExt]
+        vengono gestite dalla fallback e non qui
+        [PtrToInt,IntToPtr,BitCast,AddrSpaceCast] potrebbero portare errori*/
       ;
     }
   }
@@ -137,6 +142,7 @@ Value *FloatToFixed::convertStore(DenseMap<Value *, Value *>& op, StoreInst *sto
   return newinst;
 }
 
+
 Value *FloatToFixed::convertBinOp(DenseMap<Value *, Value *>& op, Instruction *instr)
 {
   IRBuilder<> builder(instr->getNextNode());
@@ -167,25 +173,27 @@ Value *FloatToFixed::convertBinOp(DenseMap<Value *, Value *>& op, Instruction *i
     : nullptr;
 }
 
+
 Value *FloatToFixed::fallback(DenseMap<Value *, Value *>& op, Instruction *unsupp)
 {
   Value *fallval;
   Instruction *fixval;
   std::vector<Value *> newops;
 
-  errs() << "[Fallback] attempt to wrap not supported operation:\n" << *unsupp << "\n";
+  DEBUG(errs() << "[Fallback] attempt to wrap not supported operation:\n" << *unsupp << "\n");
+
 
   for (int i=0,n=unsupp->getNumOperands();i<n;i++) {
     fallval = unsupp->getOperand(i);
 
     Value *cvtfallval = op[fallval];
-    if (cvtfallval == ConversionError) {
-      errs() << "  bail out on missing operand " << i+1 << " of " << n << "\n";
+    if (cvtfallval == ConversionError || fallval->getType()->isPointerTy()) {
+      DEBUG(errs() << "  bail out on missing operand " << i+1 << " of " << n << "\n");
       return nullptr;
     }
 
     //se è stato precedentemente sostituito e non è un puntatore
-    if (cvtfallval && !fallval->getType()->isPointerTy()) {
+    if (cvtfallval) {
       /*Nel caso in cui la chiave (valore rimosso in precedenze) è un float
         il rispettivo value è un fix che deve essere convertito in float per retrocompatibilità.
         Se la chiave non è un float allora uso il rispettivo value associato così com'è.*/
@@ -193,7 +201,7 @@ Value *FloatToFixed::fallback(DenseMap<Value *, Value *>& op, Instruction *unsup
         ? dyn_cast<Instruction>(genConvertFixToFloat(cvtfallval,fallval->getType()))
         : dyn_cast<Instruction>(cvtfallval);
 
-      errs() << "  Substituted operand number : " << i+1 << " of " << n << "\n";
+      DEBUG(errs() << "  Substituted operand number : " << i+1 << " of " << n << "\n");
       newops.push_back(fixval);
     } else {
       newops.push_back(fallval);
@@ -203,7 +211,7 @@ Value *FloatToFixed::fallback(DenseMap<Value *, Value *>& op, Instruction *unsup
   for (int i=0, n=unsupp->getNumOperands(); i<n; i++) {
     unsupp->setOperand(i, newops[i]);
   }
-  errs() << "  mutated operands to:\n" << *unsupp << "\n";
+  DEBUG(errs() << "  mutated operands to:\n" << *unsupp << "\n");
   if (unsupp->getType()->isFloatingPointTy()) {
     Value *fallbackv = genConvertFloatToFix(unsupp);
     if (unsupp->hasName())

LLVMFloatToFixed/LLVMFloatToFixedPass.cpp
@@ -23,21 +23,23 @@ static RegisterPass<FloatToFixed> X(
 
 bool FloatToFixed::runOnModule(Module &m)
 {
-  printAnnotatedObj(m);
-  
+  DEBUG_WITH_TYPE(DEBUG_ANNOTATION,printAnnotatedObj(m));
+
+
   auto roots = readAllLocalAnnotations(m);
   std::vector<Value*> rootsa(roots.begin(), roots.end());
   auto vals = buildConversionQueueForRootValues(rootsa);
-  
-  errs() << "conversion queue:\n";
-  for (Value *val: vals) {
-    val->print(outs());
-    errs() << "\n";
-  }
-  errs() << "\n\n";
-  
+
+  DEBUG(errs() << "conversion queue:\n";
+        for (Value *val: vals) {
+          val->print(outs());
+          errs() << "\n";
+        }
+        errs() << "\n\n";);
+
+
   performConversion(m, vals);
-  
+
   return true;
 }
 
@@ -46,7 +48,7 @@ std::vector<Value*> FloatToFixed::buildConversionQueueForRootValues(const ArrayR
 {
   std::vector<Value*> queue(val);
   size_t next = 0;
-  
+
   while (next < queue.size()) {
     Value *v = queue.at(next);
     for (auto *u: v->users()) {
@@ -63,7 +65,7 @@ std::vector<Value*> FloatToFixed::buildConversionQueueForRootValues(const ArrayR
     }
     next++;
   }
-  
+
   return queue;
 }
 

LLVMFloatToFixed/LLVMFloatToFixedPass.h
@@ -4,7 +4,7 @@
 #include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/DenseMap.h"
-
+#include "llvm/Support/Debug.h"
 
 #ifndef __LLVM_FLOAT_TO_FIXED_PASS_H__
 #define __LLVM_FLOAT_TO_FIXED_PASS_H__
@@ -12,6 +12,9 @@
 
 #define N_ANNO_VAR 32
 #define NO_FLOAT_ANNO "no_float"
+
+#define DEBUG_TYPE "flttofix"
+#define DEBUG_ANNOTATION "annotation"
 
 
 namespace flttofix {

test/magiclang.sh
@@ -27,7 +27,7 @@ PASSLIB="$ROOT/../build/LLVMFloatToFixed/Debug/LLVMFloatToFixed.$SOEXT"
 OUTNAME=$(echo "$1" | sed -E 's/\.[^\.]$//')
 
 $CLANG -S -emit-llvm "$1" -o "_tmp0.$1.ll"
-$OPT -load="$PASSLIB" -S -flttofix -dce "_tmp0.$1.ll" -o "_tmp1.$1.ll"
+$OPT -load="$PASSLIB" -S -flttofix -debug-only=flttofix -dce "_tmp0.$1.ll" -o "_tmp1.$1.ll"
 $LLC -o "_tmp2.$1.o" "_tmp1.$1.ll" -filetype=obj
 $CLANG -o "$OUTNAME" "_tmp2.$1.o"
 

commit c18707516ac5cc9b66c52f1df9315642feba9ea0

Fix a bug introduced in e0e8b1e that caused printfs not to be converted anymore.
Note that the original code before the bug was introduced did not consider that pointers could be converted; but now they are and the new code handles this case.

LLVMFloatToFixed/Conversion.cpp
@@ -42,7 +42,8 @@ void FloatToFixed::performConversion(Module& m, const std::vector<Value*>& q)
   }
 
   /* remove all successfully converted stores. MAY PRODUCE INCORRECT RESULTS */
-  //TODO: better logic here!!
+  /* TODO: better logic here!! correct logic would be to remove all stores to
+   * a float only if all of them were correctly converted */
   for (Value *v: q) {
     auto *i = dyn_cast<StoreInst>(v);
     auto *convi = convertedPool[v];
@@ -417,6 +418,7 @@ Value *FloatToFixed::fallback(DenseMap<Value *, Value *>& op, Instruction *unsup
 
     Value *cvtfallval = op[fallval];
     if (cvtfallval == ConversionError || fallval->getType()->isPointerTy()) {
+    if (cvtfallval == ConversionError || (cvtfallval && cvtfallval->getType()->isPointerTy())) {
       DEBUG(dbgs() << "  bail out on missing operand " << i+1 << " of " << n << "\n");
       return nullptr;
     }