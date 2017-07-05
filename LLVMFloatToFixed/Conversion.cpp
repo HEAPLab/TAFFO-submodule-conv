HEAPLab/TAFFO-submodule-conv

commit c18707516ac5cc9b66c52f1df9315642feba9ea0

Fix a bug introduced in e0e8b1e that caused printfs not to be converted anymore.
Note that the original code before the bug was introduced did not consider that pointers could be converted; but now they are and the new code handles this case.

LLVMFloatToFixed/Conversion.cpp
@@ -42,7 +42,8 @@ void FloatToFixed::performConversion(Module& m, const std::vector<Value*>& q)
   }
 
   /* remove all successfully converted stores. MAY PRODUCE INCORRECT RESULTS */
-  //TODO: better logic here!!
+  /* TODO: better logic here!! correct logic would be to remove all stores to
+   * a float only if all of them were correctly converted */
   for (Value *v: q) {
     auto *i = dyn_cast<StoreInst>(v);
     auto *convi = convertedPool[v];
@@ -417,6 +418,7 @@ Value *FloatToFixed::fallback(DenseMap<Value *, Value *>& op, Instruction *unsup
 
     Value *cvtfallval = op[fallval];
     if (cvtfallval == ConversionError || fallval->getType()->isPointerTy()) {
+    if (cvtfallval == ConversionError || (cvtfallval && cvtfallval->getType()->isPointerTy())) {
       DEBUG(dbgs() << "  bail out on missing operand " << i+1 << " of " << n << "\n");
       return nullptr;
     }

commit 3738887a6d710edd230f3e64a8ab57e7759080f5

Fix a bug introduced in c187075 caused by incorrect staging -_-

LLVMFloatToFixed/Conversion.cpp
@@ -417,7 +417,6 @@ Value *FloatToFixed::fallback(DenseMap<Value *, Value *>& op, Instruction *unsup
     fallval = unsupp->getOperand(i);
 
     Value *cvtfallval = op[fallval];
-    if (cvtfallval == ConversionError || fallval->getType()->isPointerTy()) {
     if (cvtfallval == ConversionError || (cvtfallval && cvtfallval->getType()->isPointerTy())) {
       DEBUG(dbgs() << "  bail out on missing operand " << i+1 << " of " << n << "\n");
       return nullptr;