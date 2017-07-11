414d967d3760af016382677d41839ea46d5a6a4b
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2017-07-11 19:17:46 +0200

    Improve support for release builds of LLVM and CMake Makefile builds.

--- test/magiclang.sh
+++ test/magiclang.sh
@@ -24,10 +24,19 @@ CLANG=$LLVM_DIR/bin/clang
 OPT=$LLVM_DIR/bin/opt
 LLC=$LLVM_DIR/bin/llc
 PASSLIB="$ROOT/../build/LLVMFloatToFixed/Debug/LLVMFloatToFixed.$SOEXT"
+if [ ! -e "$PASSLIB" ]; then
+  PASSLIB="$ROOT/../build/LLVMFloatToFixed/LLVMFloatToFixed.$SOEXT";
+fi
 OUTNAME=$(echo "$1" | sed -E 's/\.[^\.]$//')
 
+ISDEBUG=$("$OPT" --version | grep DEBUG | wc -l)
+DEBUGONLYFLAG="-debug-only=flttofix"
+if [ $ISDEBUG != '1' ]; then
+  DEBUGONLYFLAG='';
+fi
+
 $CLANG -S -emit-llvm "$1" -o "_tmp0.$1.ll" $3
-$OPT -load="$PASSLIB" -S -flttofix -debug-only=flttofix -dce "_tmp0.$1.ll" -o "_tmp1.$1.ll"
+$OPT -load="$PASSLIB" -S -flttofix -dce $DEBUGONLYFLAG "_tmp0.$1.ll" -o "_tmp1.$1.ll"
 $CLANG -S -o "_tmp2.$1.s" "_tmp1.$1.ll" $2 $3
 $CLANG -o "$OUTNAME" "_tmp2.$1.s" $2 $3
 
