79054a89253a7b840204f86bf1128eae0a9cb6af
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2017-08-19 01:12:04 +0200

    Reduce required stack size for durbin & ludcmp

--- test/polybench-3.1/compile_linear-algebra_solvers.sh
+++ test/polybench-3.1/compile_linear-algebra_solvers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo durbin
-./magiclang.sh 'linear-algebra/solvers/durbin/durbin.c' '-O3' '-I utilities -I linear-algebra/solvers/durbin -DPOLYBENCH_TIME -DLARGE_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'durbin_out' 'utilities/polybench.c' '-fixpfracbitsamt=8 -fixpbitsamt=32' 
+./magiclang.sh 'linear-algebra/solvers/durbin/durbin.c' '-O3' '-I utilities -I linear-algebra/solvers/durbin -DPOLYBENCH_TIME -DSTANDARD_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'durbin_out' 'utilities/polybench.c' '-fixpfracbitsamt=8 -fixpbitsamt=32' 
 
 echo dynprog
 ./magiclang.sh 'linear-algebra/solvers/dynprog/dynprog.c' '-O3' '-I utilities -I linear-algebra/solvers/dynprog -DPOLYBENCH_TIME -DSTANDARD_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'dynprog_out' 'utilities/polybench.c' '-fixpfracbitsamt=16 -fixpbitsamt=32' 
@@ -13,4 +13,4 @@ echo lu
 ./magiclang.sh 'linear-algebra/solvers/lu/lu.c' '-O3' '-I utilities -I linear-algebra/solvers/lu -DPOLYBENCH_TIME -DSTANDARD_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'lu_out' 'utilities/polybench.c' '-fixpfracbitsamt=20 -fixpbitsamt=32' 
 
 echo ludcmp
-./magiclang.sh 'linear-algebra/solvers/ludcmp/ludcmp.c' '-O3' '-I utilities -I linear-algebra/solvers/ludcmp -DPOLYBENCH_TIME -DLARGE_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'ludcmp_out' 'utilities/polybench.c' '-fixpfracbitsamt=16 -fixpbitsamt=32'
+./magiclang.sh 'linear-algebra/solvers/ludcmp/ludcmp.c' '-O3' '-I utilities -I linear-algebra/solvers/ludcmp -DPOLYBENCH_TIME -DSTANDARD_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'ludcmp_out' 'utilities/polybench.c' '-fixpfracbitsamt=16 -fixpbitsamt=32'
