dcec060f70caaa0b8c54ad50fbcca2d9729c8508
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2017-08-18 22:57:30 +0200

    Fix inconsistency of trisolv

--- test/polybench-3.1/linear-algebra/kernels/trisolv/trisolv.c
+++ test/polybench-3.1/linear-algebra/kernels/trisolv/trisolv.c
@@ -31,7 +31,7 @@ void init_array(int n,
     {
       c[i] = x[i] = ((DATA_TYPE) i) / n;
       for (j = 0; j < n; j++)
-	A[i][j] = ((DATA_TYPE) (i+1)*(j+1)) / n;
+	      A[i][j] = ((DATA_TYPE) (i+1)*(j+1)) / (n*n) + 1.0;
     }
 }
 
