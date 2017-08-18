ff35fdb7fcbabc8cabb06bd29bd4b0eba6b7bc30
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2017-08-19 01:41:24 +0200

    Reduce needed stack size in various places

--- test/polybench-3.1/compile_stencil.sh
+++ test/polybench-3.1/compile_stencil.sh
@@ -4,7 +4,7 @@ echo adi
 ./magiclang.sh 'stencils/adi/adi.c' '-O3' '-I utilities -I stencils/adi -lm -DPOLYBENCH_TIME -DMINI_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'adi_out' 'utilities/polybench.c' '-fixpfracbitsamt=32 -fixpbitsamt=64' 
 
 echo fdtd-2d
-./magiclang.sh 'stencils/fdtd-2d/fdtd-2d.c' '-O3' '-I utilities -I stencils/fdtd-2d -DPOLYBENCH_TIME -DSTANDARD_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'fdtd-2d_out' 'utilities/polybench.c' '-fixpfracbitsamt=24 -fixpbitsamt=32' 
+./magiclang.sh 'stencils/fdtd-2d/fdtd-2d.c' '-O3' '-I utilities -I stencils/fdtd-2d -DPOLYBENCH_TIME -DSMALL_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'fdtd-2d_out' 'utilities/polybench.c' '-fixpfracbitsamt=24 -fixpbitsamt=32' 
 
 echo fdtd-apml
 ./magiclang.sh 'stencils/fdtd-apml/fdtd-apml.c' '-O3' '-I utilities -I stencils/fdtd-apml -DPOLYBENCH_TIME -DMINI_DATASET -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_STACK_ARRAYS' '' 'fdtd-apml_out' 'utilities/polybench.c' '-fixpfracbitsamt=16 -fixpbitsamt=32' 
