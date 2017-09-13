70fa885b16964707834b03a5c4dcdc5153f97c1f
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2017-09-14 00:06:13 +0200

    Yet another bug caused by extreme rushing

--- test/polybench-3.1/run_experiment.sh
+++ test/polybench-3.1/run_experiment.sh
@@ -18,16 +18,16 @@ NAME=$1
 main() {
   if [ "x$NO32" = 'x' ]; then
     echo Compiling 32 bit
-    ./compile_everything.sh 2> $NAME_build.log
+    ./compile_everything.sh 2> "${NAME}_build.log"
     echo Running 32 bit
-    ./chkval_everything_better.py $TRIES > $NAME_std.txt
+    ./chkval_everything_better.py $TRIES > "${NAME}_std.txt"
     mv raw-times raw-times-32;
   fi
   if [ "x$NO64" = 'x' ]; then
     echo Compiling 64 bit
-    ./compile_everything.sh 64bit 2>> $NAME_build.log
+    ./compile_everything.sh 64bit 2>> "${NAME}_build.log"
     echo Running 64 bit
-    ./chkval_everything_better.py $TRIES > $NAME_64.txt
+    ./chkval_everything_better.py $TRIES > "${NAME}_64.txt"
     mv raw-times raw-times-64;
   fi
 }
