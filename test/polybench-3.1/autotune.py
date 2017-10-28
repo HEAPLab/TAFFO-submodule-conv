1dbd8d11990a2f4e5751c91091afe70759b0e03b
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2017-10-28 17:52:46 +0200

    Tone down autotuning verbosity (can be turned on again)

--- test/polybench-3.1/autotune.py
+++ test/polybench-3.1/autotune.py
@@ -9,13 +9,18 @@ import resource
 import argparse
 
 
+def nothing(*args, **kwargs): pass
+vprint = nothing 
+nvprint = print
+
+
 def execute(command):
   import subprocess
   limit = '65532' if os.uname()[0] == 'Darwin' else 'unlimited'
   rcmd = 'ulimit -s ' + limit + '; ' + command
   pr = subprocess.run(rcmd, stdout=subprocess.DEVNULL, stdin=subprocess.DEVNULL, shell=True)
   if pr.returncode == -2:
-    print('sigint! stopping')
+    vprint('sigint! stopping')
     os.exit(0)
   return pr.returncode
 
@@ -38,26 +43,28 @@ def errorMetric(flo, fix):
   return maxdiff / maxflo
   
   
-flovals = None
+flovals_cache = {}
 em_cache = {}
 def buildExecuteAndGetErrorMetric(benchname, fracbsize, bitness, doubleflt=False):
-  global flovals
+  global flovals_cache
   global em_cache
   
   cachekey = 'bench='+benchname+';fbs='+str(fracbsize)
   cached = em_cache.get(cachekey)
   if not (cached is None):
-    print(cachekey, ' is cached')
+    vprint(cachekey, ' is cached')
     return cached
     
   execute('./compile_everything.sh --only=%s --frac=%d --tot=%d %s 2>> ' \
           'build.log > /dev/null' % (benchname, fracbsize, bitness, \
           '64bit' if doubleflt else ''))
   
+  flovals = flovals_cache.get(benchname)
   if flovals is None:
     fn = './output-data-32/%s_out_not_opt.output.csv' % benchname
     execute('./build/%s_out_not_opt 2> %s > /dev/null' % (benchname, fn))
     flovals = [float(v) for v in ReadValues(fn)]
+    flovals_cache[benchname] = flovals
     
   fn = './output-data-32/%s_out.output.csv' % benchname
   res = execute('./build/%s_out 2> %s > /dev/null' % (benchname, fn))
@@ -76,9 +83,10 @@ def plotErrorMetric(benchname, bitness=32, doubleflt=False):
   fracbits = range(bitness)
   err = []
   for b in fracbits:
-    print(b)
+    vprint(b)
     err += [buildExecuteAndGetErrorMetric(benchname, b, bitness, doubleflt)]
-    print(err)
+    vprint(err)
+  print(err)
   plt.plot(fracbits, err)
   plt.yscale('log')
   plt.show()
@@ -87,7 +95,7 @@ def plotErrorMetric(benchname, bitness=32, doubleflt=False):
 def autotune(benchname, bitness=32, doubleflt=False):
   steps = 0
   
-  print('## lwall & rwall search')
+  vprint('## lwall & rwall search')
   lwall, rwall = -1, bitness
   minimum = 1
   
@@ -95,40 +103,40 @@ def autotune(benchname, bitness=32, doubleflt=False):
   while len(queue) > 0:
     lwall, rwall = queue.pop(0)
     center = int((lwall + rwall) / 2)
-    print('lwall, center, rwall = ', lwall, center, rwall)
+    vprint('lwall, center, rwall = ', lwall, center, rwall)
     if rwall - lwall <= 1:
-      print('empty interval')
+      vprint('empty interval')
       continue
     queue += [(lwall, center), (center, rwall)]
     
     minimum = buildExecuteAndGetErrorMetric(benchname, center, bitness, doubleflt)
-    print('minimum = ', minimum)
+    vprint('minimum = ', minimum)
     steps += 1
     if minimum < 0.1:
-      print('ok!')
+      vprint('ok!')
       break
-  print('lwall = ', lwall, '; rwall = ', rwall)
+  vprint('lwall = ', lwall, '; rwall = ', rwall)
   
-  print('## min search')
+  vprint('## min search')
   while rwall - lwall > 1:
     center = int((lwall + rwall) / 2)
-    print('lwall, center, rwall = ', lwall, center, rwall)
+    vprint('lwall, center, rwall = ', lwall, center, rwall)
     sample = buildExecuteAndGetErrorMetric(benchname, center, bitness, doubleflt)
-    print('sample = ', sample)
+    vprint('sample = ', sample)
     steps += 1
     if sample <= minimum:
       lwall = center
       minimum = sample
-      print('sample is new best')
+      vprint('sample is new best')
     else:
       rwall = center
-      print('sample is new rwall')
+      vprint('sample is new rwall')
       if sample <= 0.1:
-        print('(spike at ', center, ')')
+        vprint('(spike at ', center, ')')
       
-  print('optimal frac bits = ', lwall, '; total # iterations = ', steps)
-  print('worst value error = ', minimum)  
-  return lwall
+  vprint('optimal frac bits = ', lwall, '; total # iterations = ', steps)
+  vprint('worst value error = ', minimum)  
+  return lwall, steps
   
   
 parser = argparse.ArgumentParser(description='autotune for polybench')
@@ -142,13 +150,21 @@ parser.add_argument('--bit', dest='bitness', type=int,
 parser.add_argument('--flt-double', dest='double', action='store_true',
                     help='compare fixed point with double-precision floats '
                     'instead than with single-precision floats')
+parser.add_argument('--verbose', action='store_true',
+                    help='be verbose')
 args = parser.parse_args()
 
+if args.verbose:
+  vprint = print
+  nvprint = nothing
+
 for bench_name in args.benchnames:
+  vprint('#### ', bench_name)
   if args.plot:
     plotErrorMetric(bench_name, args.bitness, args.double)
   else:
-    autotune(bench_name, args.bitness, args.double)
-
+    res, steps = autotune(bench_name, args.bitness, args.double)
+    nvprint(bench_name, res, steps)
+  vprint('\n')
 
 
