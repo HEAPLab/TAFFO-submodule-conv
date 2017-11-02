f478d2c1bb839abd3a1913426ab13370fd24964c
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2017-11-02 13:20:43 +0100

    Fix some mistakes in the configuration files.

--- test/polybench-c-4.2.1-beta/compile_datamining.sh
+++ test/polybench-c-4.2.1-beta/compile_datamining.sh
@@ -3,5 +3,5 @@
 source common_compile.sh
 
 ROOT='datamining'
-compile "correlation" $D_STANDARD_DATASET 22 32 59 64
-compile "covariance" $D_STANDARD_DATASET  10 32 51 64
+compile "correlation" $D_STANDARD_DATASET 22 32 54 64
+compile "covariance" $D_STANDARD_DATASET  10 32 42 64
