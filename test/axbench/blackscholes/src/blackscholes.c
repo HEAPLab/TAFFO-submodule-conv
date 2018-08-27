2d95d292418167685f04ad315de49affdbb19db3
Antonio Di Bello <antonio.dibello@mail.polimi.it>
2018-08-27 12:57:07 +0200

    Axbench/blackscholes annotations

--- test/axbench/blackscholes/src/blackscholes.c
+++ test/axbench/blackscholes/src/blackscholes.c
@@ -136,10 +136,10 @@ fptype CNDF ( fptype InputX )
 //////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////
-fptype BlkSchlsEqEuroNoDiv( fptype sptprice __attribute((annotate("no_float 39 19"))) ,
-                            fptype strike __attribute((annotate("no_float 40 19"))) , fptype rate __attribute((annotate("no_float 30 18"))), 
-                            fptype volatility __attribute((annotate("no_float 30 54"))), fptype time __attribute((annotate("no_float 24 24"))), 
-                            int otype, float timet __attribute((annotate("no_float 39 15"))), 
+fptype BlkSchlsEqEuroNoDiv( fptype sptprice __attribute((annotate("no_float 20 12"))) ,
+                            fptype strike __attribute((annotate("no_float 16 16"))) , fptype rate __attribute((annotate("no_float 8 24"))), 
+                            fptype volatility __attribute((annotate("no_float 8 24"))), fptype time __attribute((annotate("no_float 20 12"))), 
+                            int otype, float timet __attribute((annotate("no_float 20 12"))), 
                             fptype*  N1, fptype* N2)
 {
     fptype OptionPrice;
