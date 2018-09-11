6510d66fb5bb9ad1fb770fbe0f5d37f2790e0015
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-09-11 03:36:54 +0200

    Fix configuration of jacobi-2d

--- test/polybench-c-4.2.1-beta/stencils/jacobi-2d/jacobi-2d.h
+++ test/polybench-c-4.2.1-beta/stencils/jacobi-2d/jacobi-2d.h
@@ -61,7 +61,7 @@
 
 #if defined(CONF_BAD)
 #define ANN_1 __attribute__((annotate("no_float")))
-#define ANN_2 __attribute__((annotate("no_float 7 25")))
+#define ANN_2 __attribute__((annotate("no_float 13 19")))
 #elif defined(CONF_WORSE)
 #define ANN_1
 #define ANN_2 __attribute__((annotate("no_float")))
