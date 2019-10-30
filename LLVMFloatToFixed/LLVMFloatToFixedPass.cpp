HEAPLab/TAFFO-submodule-conv

commit 6acbd1f55605900e05c6ff87bc77815108d9638d

Convert functions reached from non-converted instructions.
The operands might be converted and the function might have been cloned.

LLVMFloatToFixed/LLVMFloatToFixedPass.cpp
@@ -46,8 +46,8 @@ bool FloatToFixed::runOnModule(Module &m)
   vals.insert(vals.begin(), global.begin(), global.end());
   MetadataCount = vals.size();
 
-  propagateCall(vals, global);
   sortQueue(vals);
+  propagateCall(vals, global);
   LLVM_DEBUG(printConversionQueue(vals));
   ConversionCount = vals.size();
 
@@ -343,7 +343,7 @@ void FloatToFixed::propagateCall(std::vector<Value *> &vals, llvm::SmallPtrSetIm
     SmallVector<ReturnInst*,100> returns;
     CloneFunctionInto(newF, oldF, mapArgs, true, returns);
     
-    SmallPtrSet<Value *, 32> newVals; //propagate fixp conversion
+    std::vector<Value *> newVals; //propagate fixp conversion
     oldIt = oldF->arg_begin();
     newIt = newF->arg_begin();
     for (int i=0; oldIt != oldF->arg_end() ; oldIt++, newIt++,i++) {
@@ -370,17 +370,17 @@ void FloatToFixed::propagateCall(std::vector<Value *> &vals, llvm::SmallPtrSetIm
         operandPool[placehValue] = newIt;
         
         valueInfo(placehValue)->isArgumentPlaceholder = true;
-        newVals.insert(placehValue);
+        newVals.push_back(placehValue);
         
         /* No need to mark the argument itself, readLocalMetadata will
          * do it in a bit as its metadata has been cloned as well */
       }
     }
     
-    newVals.insert(global.begin(), global.end());
+    newVals.insert(newVals.end(), global.begin(), global.end());
     SmallPtrSet<Value*, 32> localFix;
     readLocalMetadata(*newF, localFix);
-    newVals.insert(localFix.begin(), localFix.end());
+    newVals.insert(newVals.end(), localFix.begin(), localFix.end());
     
     /* Make sure that the new arguments have correct ValueInfo */
     oldIt = oldF->arg_begin();
@@ -396,11 +396,14 @@ void FloatToFixed::propagateCall(std::vector<Value *> &vals, llvm::SmallPtrSetIm
     for (ReturnInst *v : returns) {
       if (!hasInfo(call.getInstruction()))
         continue;
-      newVals.insert(v);
+      newVals.push_back(v);
       demandValueInfo(v)->fixpType = valueInfo(call.getInstruction())->fixpType;
       valueInfo(v)->origType = nullptr;
       valueInfo(v)->fixpTypeRootDistance = 0;
     }
+    
+    LLVM_DEBUG(dbgs() << "Sorting queue of new function " << newF->getName() << "\n");
+    sortQueue(newVals);
     
     oldFuncs.insert(oldF);
     
@@ -479,10 +482,16 @@ Function* FloatToFixed::createFixFun(CallSite* call, bool *old)
     return newF;
   }
   if (old) *old = false;
+  
+  Type *retType = oldF->getReturnType();
+  if (hasInfo(call->getInstruction())) {
+    if (!valueInfo(call->getInstruction())->noTypeConversion)
+      retType = getLLVMFixedPointTypeForFloatValue(call->getInstruction());
+  }
 
   FunctionType *newFunTy = FunctionType::get(
       isFloatType(oldF->getReturnType()) ?
-      getLLVMFixedPointTypeForFloatValue(call->getInstruction()) :
+      retType :
       oldF->getReturnType(),
       typeArgs, oldF->isVarArg());
 

commit e882ebcc67456b3569cb3f321acd85a9edfd5a2a

Fix handling of PHINodes which broke in 6acbd1f

LLVMFloatToFixed/LLVMFloatToFixedPass.cpp
@@ -81,7 +81,6 @@ void FloatToFixed::openPhiLoop(PHINode *phi)
     return;
   }
   
-  info.phi = phi;
   info.placeh_noconv = createPlaceholder(phi->getType(), phi->getParent(), "phi_noconv");
   *(newValueInfo(info.placeh_noconv)) = *(valueInfo(phi));
   phi->replaceAllUsesWith(info.placeh_noconv);
@@ -98,7 +97,7 @@ void FloatToFixed::openPhiLoop(PHINode *phi)
   
   LLVM_DEBUG(dbgs() << "created placeholder (non-converted=[" << *info.placeh_noconv << "], converted=[" << *info.placeh_conv << "]) for phi " << *phi << "\n");
   
-  phiReplacementData.push_back(info);
+  phiReplacementData[phi] = info;
 }
 
 
@@ -106,8 +105,9 @@ void FloatToFixed::closePhiLoops()
 {
   LLVM_DEBUG(dbgs() << __PRETTY_FUNCTION__ << " begin\n");
   
-  for (PHIInfo& info: phiReplacementData) {
-    PHINode *origphi = info.phi;
+  for (auto data: phiReplacementData) {
+    PHINode *origphi = data.first;
+    PHIInfo& info = data.second;
     Value *substphi = operandPool[origphi];
     
     LLVM_DEBUG(dbgs() << "restoring data flow of phi " << *origphi << "\n");
@@ -299,6 +299,7 @@ void FloatToFixed::cleanup(const std::vector<Value*>& q)
   
   /* remove old phis manually as DCE cannot remove values having a circular
    * dependence on a phi */
+  phiReplacementData.clear();
   clear(isa<PHINode>);
 
   for (Instruction *v: toErase) {
@@ -424,8 +425,13 @@ void FloatToFixed::propagateCall(std::vector<Value *> &vals, llvm::SmallPtrSetIm
     Value *val = vals[removej];
     bool toDelete = false;
     if (Instruction *inst = dyn_cast<Instruction>(val)) {
-      if (oldFuncs.count(inst->getFunction()))
+      if (oldFuncs.count(inst->getFunction())) {
         toDelete = true;
+        if (PHINode *phi = dyn_cast_or_null<PHINode>(inst)) {
+          /* the new phis are added by sortQueue() */
+          phiReplacementData.erase(phi);
+        }
+      }
     } else if (Argument *arg = dyn_cast<Argument>(val)) {
       if (oldFuncs.count(arg->getParent()))
         toDelete = true;

LLVMFloatToFixed/LLVMFloatToFixedPass.h
@@ -6,6 +6,7 @@
 #include "llvm/ADT/SmallSet.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/DenseMap.h"
+#include "llvm/IR/ValueMap.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Support/CommandLine.h"
@@ -62,7 +63,6 @@ struct ValueInfo {
 
 
 struct PHIInfo {
-  llvm::PHINode *phi;
   llvm::Value *placeh_noconv;
   llvm::Value *placeh_conv;
 };
@@ -86,7 +86,7 @@ struct FloatToFixed : public llvm::ModulePass {
   /* to not be accessed directly, use valueInfo() */
   llvm::DenseMap<llvm::Value *, std::shared_ptr<ValueInfo>> info;
   
-  llvm::SmallVector<PHIInfo, 8> phiReplacementData;
+  llvm::ValueMap<llvm::PHINode *, PHIInfo> phiReplacementData;
   
   FloatToFixed(): ModulePass(ID) { };
   void getAnalysisUsage(llvm::AnalysisUsage &) const override;