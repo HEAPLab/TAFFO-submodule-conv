HEAPLab/TAFFO-submodule-conv

commit 6acbd1f55605900e05c6ff87bc77815108d9638d

Convert functions reached from non-converted instructions.
The operands might be converted and the function might have been cloned.

LLVMFloatToFixed/LLVMFloatToFixedPass.cpp
@@ -46,8 +46,8 @@ bool FloatToFixed::runOnModule(Module &m)
   vals.insert(vals.begin(), global.begin(), global.end());
   MetadataCount = vals.size();
 
-  propagateCall(vals, global);
   sortQueue(vals);
+  propagateCall(vals, global);
   LLVM_DEBUG(printConversionQueue(vals));
   ConversionCount = vals.size();
 
@@ -343,7 +343,7 @@ void FloatToFixed::propagateCall(std::vector<Value *> &vals, llvm::SmallPtrSetIm
     SmallVector<ReturnInst*,100> returns;
     CloneFunctionInto(newF, oldF, mapArgs, true, returns);
     
-    SmallPtrSet<Value *, 32> newVals; //propagate fixp conversion
+    std::vector<Value *> newVals; //propagate fixp conversion
     oldIt = oldF->arg_begin();
     newIt = newF->arg_begin();
     for (int i=0; oldIt != oldF->arg_end() ; oldIt++, newIt++,i++) {
@@ -370,17 +370,17 @@ void FloatToFixed::propagateCall(std::vector<Value *> &vals, llvm::SmallPtrSetIm
         operandPool[placehValue] = newIt;
         
         valueInfo(placehValue)->isArgumentPlaceholder = true;
-        newVals.insert(placehValue);
+        newVals.push_back(placehValue);
         
         /* No need to mark the argument itself, readLocalMetadata will
          * do it in a bit as its metadata has been cloned as well */
       }
     }
     
-    newVals.insert(global.begin(), global.end());
+    newVals.insert(newVals.end(), global.begin(), global.end());
     SmallPtrSet<Value*, 32> localFix;
     readLocalMetadata(*newF, localFix);
-    newVals.insert(localFix.begin(), localFix.end());
+    newVals.insert(newVals.end(), localFix.begin(), localFix.end());
     
     /* Make sure that the new arguments have correct ValueInfo */
     oldIt = oldF->arg_begin();
@@ -396,11 +396,14 @@ void FloatToFixed::propagateCall(std::vector<Value *> &vals, llvm::SmallPtrSetIm
     for (ReturnInst *v : returns) {
       if (!hasInfo(call.getInstruction()))
         continue;
-      newVals.insert(v);
+      newVals.push_back(v);
       demandValueInfo(v)->fixpType = valueInfo(call.getInstruction())->fixpType;
       valueInfo(v)->origType = nullptr;
       valueInfo(v)->fixpTypeRootDistance = 0;
     }
+    
+    LLVM_DEBUG(dbgs() << "Sorting queue of new function " << newF->getName() << "\n");
+    sortQueue(newVals);
     
     oldFuncs.insert(oldF);
     
@@ -479,10 +482,16 @@ Function* FloatToFixed::createFixFun(CallSite* call, bool *old)
     return newF;
   }
   if (old) *old = false;
+  
+  Type *retType = oldF->getReturnType();
+  if (hasInfo(call->getInstruction())) {
+    if (!valueInfo(call->getInstruction())->noTypeConversion)
+      retType = getLLVMFixedPointTypeForFloatValue(call->getInstruction());
+  }
 
   FunctionType *newFunTy = FunctionType::get(
       isFloatType(oldF->getReturnType()) ?
-      getLLVMFixedPointTypeForFloatValue(call->getInstruction()) :
+      retType :
       oldF->getReturnType(),
       typeArgs, oldF->isVarArg());
 

commit 490586026bacbc893dd7a37e901ac4adf5c1f08e

Fix handling of PHINodes which broke in 6acbd1f, this time FOR REAL

LLVMFloatToFixed/LLVMFloatToFixedPass.cpp
@@ -334,15 +334,16 @@ void FloatToFixed::propagateCall(std::vector<Value *> &vals, llvm::SmallPtrSetIm
     LLVM_DEBUG(dbgs() << "Converting function " << oldF->getName() << " : " << *oldF->getType()
                << " into " << newF->getName() << " : " << *newF->getType() << "\n");
     
-    ValueToValueMapTy mapArgs; // Create Val2Val mapping and clone function
+    ValueToValueMapTy origValToCloned; // Create Val2Val mapping and clone function
     Function::arg_iterator newIt = newF->arg_begin();
     Function::arg_iterator oldIt = oldF->arg_begin();
     for (; oldIt != oldF->arg_end() ; oldIt++, newIt++) {
       newIt->setName(oldIt->getName());
-      mapArgs.insert(std::make_pair(oldIt, newIt));
+      origValToCloned.insert(std::make_pair(oldIt, newIt));
     }
     SmallVector<ReturnInst*,100> returns;
-    CloneFunctionInto(newF, oldF, mapArgs, true, returns);
+    CloneFunctionInto(newF, oldF, origValToCloned, true, returns);
+    /* after CloneFunctionInto, valueMap maps all values from the oldF to the newF (not just the arguments) */
     
     std::vector<Value *> newVals; //propagate fixp conversion
     oldIt = oldF->arg_begin();
@@ -428,7 +429,6 @@ void FloatToFixed::propagateCall(std::vector<Value *> &vals, llvm::SmallPtrSetIm
       if (oldFuncs.count(inst->getFunction())) {
         toDelete = true;
         if (PHINode *phi = dyn_cast_or_null<PHINode>(inst)) {
-          /* the new phis are added by sortQueue() */
           phiReplacementData.erase(phi);
         }
       }

LLVMFloatToFixed/LLVMFloatToFixedPass.h
@@ -93,7 +93,7 @@ struct FloatToFixed : public llvm::ModulePass {
   bool runOnModule(llvm::Module &M) override;
 
   void readGlobalMetadata(llvm::Module &m, llvm::SmallPtrSetImpl<llvm::Value *> &res, bool functionAnnotation = false);
-  void readLocalMetadata(llvm::Function &f, llvm::SmallPtrSetImpl<llvm::Value *> &res);
+  void readLocalMetadata(llvm::Function &f, llvm::SmallPtrSetImpl<llvm::Value *> &res, bool onlyArguments = false);
   void readAllLocalMetadata(llvm::Module &m, llvm::SmallPtrSetImpl<llvm::Value *> &res);
   bool parseMetaData(llvm::SmallPtrSetImpl<llvm::Value *> *variables, mdutils::MDInfo *fpInfo, llvm::Value *instr);
   void removeNoFloatTy(llvm::SmallPtrSetImpl<llvm::Value *>& res);

LLVMFloatToFixed/MDCollector.cpp
@@ -34,7 +34,7 @@ void FloatToFixed::readGlobalMetadata(Module &m, SmallPtrSetImpl<Value *> &varia
 }
 
 
-void FloatToFixed::readLocalMetadata(Function &f, SmallPtrSetImpl<Value *> &variables)
+void FloatToFixed::readLocalMetadata(Function &f, SmallPtrSetImpl<Value *> &variables, bool argumentsOnly)
 {
   MetadataManager &MDManager = MetadataManager::getMetadataManager();
 
@@ -50,6 +50,9 @@ void FloatToFixed::readLocalMetadata(Function &f, SmallPtrSetImpl<Value *> &vari
     arg++;
   }
 
+  if (argumentsOnly)
+    return;
+
   for (inst_iterator iIt = inst_begin(&f), iItEnd = inst_end(&f); iIt != iItEnd; iIt++) {
     MDInfo *MDI = MDManager.retrieveMDInfo(&(*iIt));
     if (MDI) {
@@ -62,8 +65,15 @@ void FloatToFixed::readLocalMetadata(Function &f, SmallPtrSetImpl<Value *> &vari
 void FloatToFixed::readAllLocalMetadata(Module &m, SmallPtrSetImpl<Value *> &res)
 {
   for (Function &f: m.functions()) {
+    bool argsOnly = false;
+    if (f.getMetadata(SOURCE_FUN_METADATA)) {
+      LLVM_DEBUG(dbgs() << __FUNCTION__ << " skipping function body of " << f.getName() << " because it is cloned\n");
+      functionPool[&f] = nullptr;
+      argsOnly = true;
+    }
+    
     SmallPtrSet<Value*, 32> t;
-    readLocalMetadata(f, t);
+    readLocalMetadata(f, t, argsOnly);
     res.insert(t.begin(), t.end());
 
     /* Otherwise dce pass ignore the function